4053035ba6aba458660e0ff8540b416f5165c7d8
BryanMillon <bryanmillon31@gmail.com>
2026-06-27 00:43:51 -0600

    adding SEO

--- src/components/ui/LocalSEO.tsx
+++ src/components/ui/LocalSEO.tsx
@@ -1,26 +1,34 @@
-export function LocalSEO() {
-  const schema = {
-    "@context": "https://schema.org",
-    "@type": "LocalBusiness",
-    name: "Junk Removal LLC",
-    description: "Professional junk removal in Atlanta, GA. Free estimates. Available 24/7.",
-    telephone: "+1-404-409-8928",
-    address: {
-      "@type": "PostalAddress",
-      addressLocality: "Atlanta",
-      addressRegion: "GA",
-      addressCountry: "US",
-    },
-    areaServed: { "@type": "City", name: "Atlanta" },
-    openingHours: "Mo-Su 00:00-23:59",
-    priceRange: "$$",
-    url: "https://junkremovalatlanta.com",
-  };
-
-  return (
-    <script
-      type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
-    />
-  );
-}
+const schema = {
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  name: "Junk Removal LLC",
+  description: "Professional junk removal in Atlanta, GA. Free estimates. Available 24/7.",
+  telephone: "+1-404-409-8928",
+  image: "https://tudominio.com/images/Truck.png",
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "Atlanta, GA",
+    addressLocality: "Atlanta",
+    addressRegion: "GA",
+    postalCode: "30301",
+    addressCountry: "US",
+  },
+  geo: {
+    "@type": "GeoCoordinates",
+    latitude: 33.749,
+    longitude: -84.388,
+  },
+  areaServed: [
+    { "@type": "City", name: "Atlanta" },
+    { "@type": "City", name: "Buckhead" },
+    { "@type": "City", name: "Decatur" },
+    { "@type": "City", name: "Marietta" },
+  ],
+  openingHours: "Mo-Su 00:00-23:59",
+  priceRange: "$$",
+  url: "https://tudominio.com",
+  sameAs: [
+    "https://www.facebook.com/tupagina",
+    "https://www.google.com/maps?cid=TU_CID",
+  ],
+};
